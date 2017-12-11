b17bd7a6dbb981af2fdf166ae565bc2d1fe76717
Reinard Kurokitsukami <guitaristkun@gmail.com>
2017-12-11 21:18:46 +0300

    Initial commit

---
+++ Assets/script/ObjectController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectController : MonoBehaviour {
+
+	Subject subject = new Subject ();
+	public GameObject ScoreTable;
+
+	void Start () {
+		ScoreObserver score = new ScoreObserver (ScoreTable, 0, 10, 1);
+
+		subject.AddObserver (score);
+	}
+
+	public void OnClick() {
+		Animator anim = GetComponent<Animator> ();
+		anim.Play ("Pressed");
+		subject.Notify();
+	}
+}
---
+++ Assets/script/ObserverLib.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public abstract class Observer {
+	public abstract void OnNotify ();
+}
+
+public class Subject {
+	List<Observer> observers = new List<Observer> ();
+
+	public void Notify () {
+		for (int i = 0; i < observers.Count;  i++) {
+			observers [i].OnNotify ();
+		}
+	}
+
+	public void AddObserver (Observer observer) {
+		observers.Add (observer);
+	}
+
+	public void RemoveObserver (Observer observer) {
+		observers.Remove (observer);
+	}
+}
+
+public class ScoreObserver : Observer {
+
+	int Score;
+	int Margin;
+	int ScoreMultiplier;
+	Text ScoreTableText;
+
+	public ScoreObserver (GameObject ScoreTable, int Score, int Margin, int ScoreMultiplier) {
+		this.Score = Score;
+		this.Margin = Margin;
+		this.ScoreMultiplier = ScoreMultiplier;
+		this.ScoreTableText = ScoreTable.GetComponent<Text> ();
+		ScoreTableText.text = "";
+	}
+
+	public override void OnNotify () {
+		CalcScore ();
+		PrintScore ();
+		SaveScore ();
+	}
+
+	void CalcScore () {
+		if ((Score >= Margin) && (ScoreMultiplier <= 10)) {
+			Margin = Score * (10 - ScoreMultiplier);
+			ScoreMultiplier += 1;
+		}
+		Score += ScoreMultiplier;
+	}
+
+	void PrintScore () {
+		string s = "Score: " + Score + ", x " + ScoreMultiplier;
+		Debug.Log (s);
+		ScoreTableText.text = s;
+	}
+
+	void SaveScore () {
+		
+	}
+}
