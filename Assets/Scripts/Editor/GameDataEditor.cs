3fed1d8b2bc62caab2738b4553dfecd59331f48f
Reinard Kurokitsukami <guitaristkun@gmail.com>
2017-12-14 17:13:06 +0300

    Separate File Data Loading Fixed

--- Assets/Scripts/Editor/GameDataEditor.cs
+++ Assets/Scripts/Editor/GameDataEditor.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Runtime.Remoting;
-using System.Collections;
+using System.IO;
 using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 using UnityEditor;
-using System.IO;
-using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine.Serialization;
 
 public class GameDataEditor: EditorWindow {
 
@@ -13,49 +12,38 @@ public class GameDataEditor: EditorWindow {
 	private string achievementDataFilename = GlobalData.achievementDataFilename;
 	private string highscoreDataFilename = GlobalData.highscoreDataFilename;
 	private string playerDataFilename = GlobalData.playerDataFilename;
+	private List<string> allDataList = { "allLevelData", "allAchievementData", "allHighscoreData", "allPlayerData" };
 
 	public List<LevelData> allLevelData;
 	public List<AchievementData> allAchievementData;
 	public List<HighscoreData> allHighscoreData;
 	public List<PlayerData> allPlayerData;
 
-	[MenuItem ("Window/Game Data Editor")]
-	static void Init () {
-		GameDataEditor window = (GameDataEditor)EditorWindow.GetWindow (typeof(GameDataEditor));
-		window.Show ();
+	[MenuItem ("Window/GameObject Data Editor")]
+	public static void ShowWindow () {
+		EditorWindow.GetWindow<GameDataEditor> ();
 	}
 
 	void OnGUI () {
 		if ((allLevelData != null) && (allAchievementData != null) && (allHighscoreData != null) && (allPlayerData != null)) {
-			SerializedObject serializedObject = new SerializedObject (this);
-			SerializedProperty serializedProperty;
-			serializedProperty = serializedObject.FindProperty ("allLevelData");
-			EditorGUILayout.PropertyField (serializedProperty, true);
-			serializedObject.ApplyModifiedProperties ();
-
-			serializedProperty = serializedObject.FindProperty ("allAchievementData");
-			EditorGUILayout.PropertyField (serializedProperty, true);
-			serializedObject.ApplyModifiedProperties ();
-
-			serializedProperty = serializedObject.FindProperty ("allHighscoreData");
-			EditorGUILayout.PropertyField (serializedProperty, true);
-			serializedObject.ApplyModifiedProperties ();
-
-			serializedProperty = serializedObject.FindProperty ("allPlayerData");
-			EditorGUILayout.PropertyField (serializedProperty, true);
-			serializedObject.ApplyModifiedProperties ();
-
+			Debug.Log ("Serializing...");
+			foreach (string name in allDataList) {
+				ApplyField (name);
+			}
 			if (GUILayout.Button ("Save Data")) {
 				SaveGameData ();
-			}
+			} 
 		}
 		if (GUILayout.Button ("Load Data")) {
 			LoadGameData ();
-		}
-	}
+		} 
+	} 
 
-	static void AddField<T> (ref T obj){
-		
+	static void ApplyField (string name) {
+		SerializedObject serializedObject = new SerializedObject (this);
+		SerializedProperty serializedProperty  = serializedObject.FindProperty (name);
+		EditorGUILayout.PropertyField (serializedProperty, true);
+		serializedObject.ApplyModifiedProperties ();
 	}
 
 	static void LoadFile<T> (ref T obj, string fileName) where T : new () {
