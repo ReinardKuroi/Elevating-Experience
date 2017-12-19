3454fc4096d05130f4de96038872dbec95e8a1ec
Reinard Kurokitsukami <guitaristkun@gmail.com>
2017-12-19 22:57:19 +0300

    Some UI stuff

---
+++ Assets/Scripts/Controllers/MenuController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+	public GameObject pauseUI;
+	public GameObject settingsUI;
+	public GameObject pauseButton;
+	public GameObject settingsButton;
+
+	private bool isPaused = false;
+	private Text t;
+
+	void Awake () {
+		t = pauseButton.GetComponentInChildren<Text> ();
+	}
+
+	void TogglePause () {
+		if (isPaused) {
+			t.text = "PAUSE";
+			Time.timeScale = 1f;
+		} else {
+			t.text = "PLAY";
+			Time.timeScale = 0f;
+		}
+		isPaused = !isPaused;
+	}
+
+	void ToggleUI (GameObject ui) {
+		ui.SetActive (!ui.activeSelf);
+	}
+
+	public void PauseClick () {
+		if (settingsUI.activeSelf)
+			ToggleUI (settingsUI);
+		TogglePause ();
+	}
+
+	public void SettingsClick () {
+		if (!isPaused)
+			TogglePause ();
+		ToggleUI (settingsUI);
+	}
+}
