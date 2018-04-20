a38eb9188dc16dbf6a9a6be1f671743cd790072d
Reinard Kurokitsukami <guitaristkun@gmail.com>
2018-04-20 22:51:46 +0300

    Textures for Technical, Reflections

---
+++ Assets/Scripts/Controllers/RandomPulseLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Light))]
+
+public class RandomPulseLight : MonoBehaviour {
+
+	public float minIntensity = 0.25f;
+	public float maxIntensity = 0.5f;
+
+	private Light light;
+
+	float random;
+
+	void Start()
+	{
+		random = Random.Range(0.0f, 65535.0f);
+		light = gameObject.GetComponent<Light> ();
+	}
+
+	void Update()
+	{
+		float noise = Mathf.PerlinNoise (random, Time.time);
+		light.intensity = Mathf.Lerp (minIntensity, maxIntensity, noise);
+	}
+}
